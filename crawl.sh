5e3b81fb02dab14fe3bd2669ea7b42ce64bc6271
mnyrop <marii@nyu.edu>
2025-04-15 16:18:29 -0400

    init crawl structure

--- crawl.sh
+++ crawl.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 echo "archiving $1 to ./crawls/collections/$2"
+mkdir -p crawls/collections/$2 && touch crawls/collections/$2/.gitkeep
 docker run --rm -v $PWD/crawls:/crawls/ -it webrecorder/browsertrix-crawler crawl --allowHashUrls --url $1 --generateWACZ --collection $2
+
